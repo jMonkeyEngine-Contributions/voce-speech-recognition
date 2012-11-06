548d9df4d2972744d68a7ee6e7b91fe4bdacd340
normen667 <normen667@3a0065ea-d28b-4802-913a-ce1e546711fa>
2012-11-06 14:07:03 +0000

    - add voce speech recognition library to update center

---
+++ src/voce/plugin/docs/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@HelpSetRegistration(helpSet = "plugin-hs.xml", position = 3972)
+package voce.plugin.docs;
+
+import org.netbeans.api.javahelp.HelpSetRegistration;
